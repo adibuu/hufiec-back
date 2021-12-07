c14ade9dcc7dba1b3d770291ea6439aa2166a348
Adrian <adibu8@gmail.com>
2021-12-07 21:18:21 +0100

    Building order pdf

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -18,11 +18,14 @@ const months = [
 exports.postOrder = async (req, res, next) => {
   try {
     const data = req.body;
+    const defaultFontPath = __dirname + "/../fonts/Roboto-Regular.ttf";
 
-    const doc = new PDFDocument({
-      size: "A4",
-    });
-    const filename = "rozkaz";
+    const splitDate = data.orderDate.split("-");
+    const year = splitDate[0];
+    const month = parseInt(splitDate[1]) - 1;
+    const day = splitDate[2];
+
+    const filename = `Rozkaz L. ${data.orderNumber}/${year}`;
 
     res.setHeader("Content-type", "application/pdf");
     res.setHeader(
@@ -30,19 +33,47 @@ exports.postOrder = async (req, res, next) => {
       "inline; filename=" + filename + ".pdf"
     );
 
-    const fontpath = __dirname + "/../fonts/Roboto-Regular.ttf";
-
+    const doc = new PDFDocument({
+      size: "A4",
+      font: defaultFontPath,
+    });
     doc.info["Title"] = filename;
-    doc.font(fontpath);
 
-    const splitDate = data.orderDate.split("-");
-    const year = splitDate[0];
-    const month = parseInt(splitDate[1]) - 1;
-    const day = splitDate[2];
-
+    //Date
     doc.fontSize(10).text(`${data.town}, ${day} ${months[month]} ${year} r.`, {
       align: "right",
     });
+
+    //TroopName
+    doc.fontSize(10).text(data.troopsName, { align: "left" });
+
+    //Order number
+    doc.moveDown();
+    doc.moveDown();
+    doc
+      .fontSize(12)
+      .font(__dirname + "/../fonts/Roboto-Bold.ttf")
+      .text(`Rozkaz L. ${data.orderNumber}/${year}`, { align: "center" });
+    doc.moveDown();
+    doc.moveDown();
+
+    //OccasionalAdmission
+    if (data.occasionalAdmission) {
+      doc
+        .fontSize(10)
+        .font(defaultFontPath)
+        .text(data.occasionalAdmission, { align: "justify" });
+      doc.moveDown();
+    }
+
+    //OrderExceptions
+    if (data.orderExceptions) {
+      doc
+        .fontSize(10)
+        .font(defaultFontPath)
+        .text(data.orderExceptions, { align: "justify" });
+      doc.moveDown();
+    }
 
     doc.pipe(res);
     doc.end();
